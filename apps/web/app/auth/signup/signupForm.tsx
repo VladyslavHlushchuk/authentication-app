bc3537a24d14c3d7bc513e3e90dd89276fabb678
VladyslavHlushchuk <hlushchukvlad03@gmail.com>
2024-11-19 23:32:12 +0200

    Add Sign Up form, integrate ShadCN UI, and implement useFormStatus

---
+++ apps/web/app/auth/signup/signupForm.tsx
@@ -0,0 +1,30 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SubmitButton from "@/components/ui/submitButton";
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <form>
+            <div className="flex flex-col gap-2">
+                <div>
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" name="name" placeholder="John Doe" />
+                </div>
+
+                <div>
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" name="email" placeholder="john@example.com" />
+                </div>
+
+                <div>
+                    <Label htmlFor="password">Password</Label>
+                    <Input id="password" name="password" type="password" />
+                </div>
+                <SubmitButton>Sign Up</SubmitButton>
+            </div>
+        </form>
+    );
+}
+
+export default SignUpForm;
