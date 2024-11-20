e8b50c59c56ac9dc672bae1872fc7ba108028dbe
VladyslavHlushchuk <hlushchukvlad03@gmail.com>
2024-11-20 13:56:11 +0200

    Implement Sign Up Server Action with Zod Validation and useFormState

---
+++ apps/web/lib/type.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+export type FormState =
+  | {
+      error?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+  export const SignupFormSchema = z.object({
+    name: z
+      .string()
+      .min(2, {
+        message: "Name must be at least 2 characters long.",
+      })
+      .trim(),
+    email: z
+      .string()
+      .email({ message: "Please enter a valid email." })
+      .trim(),
+    password: z
+      .string()
+      .min(8, { message: "Be at least 8 characters long." }) // Мінімум 8 символів
+      .regex(/[A-Z]/, {
+        message: "Contain at least one uppercase letter.", // Велика літера
+      })
+      .regex(/[a-z]/, {
+        message: "Contain at least one lowercase letter.", // Мала літера
+      })
+      .regex(/[0-9]/, {
+        message: "Contain at least one number.", // Цифра
+      })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Contain at least one special character.", // Символ
+      })
+      .trim(),
+  });
+  
